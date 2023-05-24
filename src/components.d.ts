c046e393f49e6afa2c23688f90070232f3345027
studyxiao <studyxiao@163.com>
2023-05-25 01:21:44 +0800

    修复 base 错误

--- src/components.d.ts
+++ src/components.d.ts
@@ -11,9 +11,6 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Emoji: typeof import('./components/emoji.vue')['default']
     EmojiSelector: typeof import('./components/emojiSelector.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
-    ICarbonSun: typeof import('~icons/carbon/sun')['default']
-    IMyIconsVue: typeof import('~icons/my-icons/vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import generatedRoutes from '~pages'
 
 const routes = setupLayouts(generatedRoutes)
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
